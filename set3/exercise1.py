f4e5472db86e00aa0bfaba35761b51677b74f6c7
Anthony-Fr-1 <avfranco98@yahoo.com.au>
2021-06-24 13:03:40 +1000

    Ex 1 Set 3

--- set3/exercise1.py
+++ set3/exercise1.py
@@ -9,10 +9,13 @@ def loop_ranger(start, stop=None, step=1):
     """Return a list of numbers between start and stop in steps of step.
 
     Do this using any method apart from JUST using range() #TODO: clarify this wording
-    The look up the docs for range(), you can answer this with just the range 
+    The look up the docs for range(), you can answer this with just the range
     function, but we'd like you to do it the long way, probably using a loop.
     """
-    return None
+    my_range = []
+    for i in range(start, stop, step=step):
+        my_range.append(i)
+    return my_range
 
 
 def lone_ranger(start, stop, step):
@@ -20,7 +23,11 @@ def lone_ranger(start, stop, step):
 
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    return None
+    my_range = []
+    for i in range(start, stop, step=1):
+        my_range.append(i)
+    return my_range
+    
 
 
 def two_step_ranger(start, stop):
@@ -29,8 +36,10 @@ def two_step_ranger(start, stop):
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    return None
-
+    my_range = []
+    for i in range(step=2):
+        my_range.append(i)
+    return my_range
 
 def stubborn_asker(low, high):
     """Ask for a number between low and high until actually given one.
@@ -40,16 +49,38 @@ def stubborn_asker(low, high):
 
     Look up the docs for input
     """
+    import random
+
+    high = 10
+    low = 1
+    chosen_number = random.randint(1, 10)
+    while (low < high):
+        print("yes")
+    else:
+        print("no")
+    print (is chosen_number in )
     return None
 
 
 def not_number_rejector(message):
     """Ask for a number repeatedly until actually given one.
 
-    Ask for a number, and if the response is actually NOT a number 
+    Ask for a number, and if the response is actually NOT a number
     (e.g. "cow", "six", "8!") then throw it out and ask for an actual number.
     When you do get a number, return it.
     """
+    import random
+
+    high = 10
+    low = 1
+    chosen_number = random.randint(1, 10)
+    while (low < high):
+        print("yes")
+    if chosen_number == int:
+        print("yes")
+    else:
+        print("no")
+    
     return None
 
 
@@ -61,6 +92,9 @@ def super_asker(low, high):
     Try to call at least one of the other functions to minimise the
     amount of code.
     """
+
+
+
     return None
 
 
